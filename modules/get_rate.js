7dc09ab47d0da24936773426745ecd8bb24f5fee
MorphSight <irishsnowday@icloud.com>
2020-11-14 22:15:56 -0800

    Before

---
+++ modules/get_rate.js
@@ -0,0 +1,10 @@
+
+
+module.exports.getRate = function getRate(toAddress, fromAddress, cost, type){
+    var req = new XMLHttpRequest();
+    req.open("GET")
+
+    const toAddress = new api.Address({
+
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const {body, validationResult} = require('express-validator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', pageName: 'homepage' });
+});
+
+router.get('/getRate', function(req, res, next) {
+  res.render('getRate', { title: 'Express' , pageName: 'getRate'});
 });
 
 module.exports = router;
